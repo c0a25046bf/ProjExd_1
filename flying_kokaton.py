e44a7d2995ed7e2c46f407bb7888ee35c2dc0a5b
skysora <c0a25046bf@edu.teu.ac.jp>
2026-04-14 18:14:24 +0900

    コード修正

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -22,9 +22,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
-        kk_rct.move_ip((-1,0))#演習１
-
-        kk=[0,0]
+        
+        kk=[-1,0]
 
         key_lst = pg.key.get_pressed()#練習１０－３>#演習２
         if key_lst[pg.K_UP]:
